7835067a44aa6ccfc0b17c9f1f418d8719fdd271
Nat Kershaw <nakersha@microsoft.com>
2022-11-29 11:43:46 -0800

    Tidy up some comments

--- web/util/predict.ts
+++ web/util/predict.ts
@@ -1,16 +1,16 @@
-import * as ort from 'onnxruntime-web';
 import path from 'path';
-var base64js = require('base64-js')
+import * as ort from 'onnxruntime-web';
+import * as base64js from 'base64-js' 
 
 export const predict = async (image): Promise<Inference> => {
     try {
         //Find the absolute path of the model directory
         const modelDirectory = path.join(process.cwd(), 'model');
-        console.log(modelDirectory)
+
+        console.log(await image.current.decode())
     
         // TODO Move session initialization into app initialization
         console.log("Creating ORT inference session")
-        //const session = await ort.InferenceSession.create(modelDirectory + '/model.with_pre_post_processing.onnx')
         const session = await ort.InferenceSession.create('./_next/static/chunks/pages/model.with_pre_post_processing.onnx')
         console.log(session)
 
@@ -18,7 +18,7 @@ export const predict = async (image): Promise<Inference> => {
         console.log(image.current.height)
         console.log(image.current.width * image.current.height * 3)
 
-        // Pre process the image bytes
+        // Get the image bytes from the base64 string
         const uint8array = base64js.toByteArray(image.current.src.replace('data:image/png;base64,', ''))
         console.log(uint8array.length)
 
